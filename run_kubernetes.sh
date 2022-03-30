ff2782cfb1b1ea8d9bdb9ea92810e247a21730ae
arnoldrx <tagnearnold2080@gmail.com>
2022-03-30 12:49:51 +0100

    Updates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=arnoldrx/ml:api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy api --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get nodes
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/api-6555754fb5-dl5rg 8000:80
 
